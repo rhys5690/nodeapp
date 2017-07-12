ea839b47d461c445013b9a372a55d1864cc42ee1
rhys5690 <rhys5690@gmail.com>
2017-07-12 15:07:13 +1000

    .

--- Starter/app.js
+++ Starter/app.js
@@ -1,8 +1,16 @@
 // Your Javascript Code Goes Here
 
 // Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
+var Emitter = require('./emitter')
 
-var greet = require('./greet');
+var emtr = new Emitter()
 
-greet.english();
-greet.spanish();
+emtr.on('greet', function() {
+  console.log('yoyoyoo');
+});
+
+emtr.on('greet', function() {
+  console.log('a greet occurred');
+});
+
+emtr.emit('greet');
--- Starter/greet/index.js
+++
@@ -1,7 +0,0 @@
-var spanish = require('./spanish');
-var english = require('./english');
-
-module.exports = {
-  english: english,
-  spanish: spanish
-};
